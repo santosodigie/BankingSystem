9935c33fa242185dd334872b3975e0bc1e412ff0
Santos Odigie <santosodigie104@gmail.com>
2024-10-08 11:50:17 +0100

    feat: Add Account class with basic banking functionalities
    - Introduced Account class with properties: AccountNumber, AccountHolderName, and Balance.
    - Implemented constructor to initialize account details.
    - Added Deposit method to allow depositing money into the account.
    - Added Withdraw method to handle money withdrawals, ensuring valid transactions.
    - Included DisplayAccountDetails method to print account information.

---
+++ Account.cs
@@ -0,0 +1,58 @@
+using System;
+
+public class Account
+{
+    //properties
+    public int AccountNumber
+    {
+        get;
+        private set;
+    }
+    public string AccountHolderName
+    {
+        get;
+        private set;
+    }
+    public double Balance
+    {
+        get;
+        private set;
+    }
+    //constructor
+    public class Account(int accountNumber, string accountHolderName, double initialBalance)
+    {
+        AccountNumber = accountNumber;
+        AccountHolderName = accountHolderName;
+        Balance = initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine($"You have deposited {amount} New Balance is {Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Deposited amount must be positive");
+        }
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"You have withdrawn {amount}, your new Balance is {Balance}")
+        }
+        else
+        {
+            Console.WriteLine("Invalid withdrawal amount, please make sure you have enough funds!");
+        }
+    }
+    public void DisplayAccountDetails()
+    {
+        Console.WriteLine($"Account Number: {AccountNumber}, Account Holder: {AccountHolderName}, Balance: {Balance}");
+    }
+}
