d396c218b9d4a76958749ba440ea40acebb19bd0
Santos Odigie <santosodigie104@gmail.com>
2024-10-14 21:59:47 +0100

    created methods for each user option

--- Program.cs
+++ Program.cs
@@ -1,86 +1,115 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
-Bank bank = new Bank();
-bool exit = false;
-
-while (!exit)
+class Program
 {
-    Console.WriteLine("1. Create Account");
-    Console.WriteLine("2. Deposit Money");
-    Console.WriteLine("3. Withdraw Money");
-    Console.WriteLine("4. Transfer Money");
-    Console.WriteLine("5. View Account Details");
-    Console.WriteLine("6. Exit");
-    Console.WriteLine("Choose an option: ");
-
-    switch (Console.ReadLine())
+    static void Main()
     {
-        case "1":
-            Console.Write("Enter account holder name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter initial deposit: ");
-            double initialDeposit = double.Parse(Console.ReadLine());
-            bank.CreateAccount(name, initialDeposit);
-            break;
-        case "2":
-            Console.Write("Enter account number");
-            int accountNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter amount to deposit: ");
-            double amount = double.Parse(Console.ReadLine());
-            Account account = bank.FindAccount(accountNumber);
-            if (account != null)
-            {
-                account.Deposit(amount);
-            }
-            else
-            {
-                Console.WriteLine("Account not found.");
-            }
-            break;
-        case "3":
-            Console.Write("Enter account number");
-            int accountNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter ammount you want to withdraw");
-            double ammount = double.Parse(Console.ReadLine());
-            Account account = bank.FindAccount(accountNumber);
-            if (account != null)
-            {
-                account.Withdraw(ammount);
-            }
-            else
-            {
-                Console.WriteLine("Account not found.");
-            }
-            break;
+        Bank bank = new Bank();
+        bool exit = false;
 
-        case "4":
-            Console.Write("Enter the source account number: ");
-            int fromAccountNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter the destination account number: ");
-            int toAccountNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter amount to transfer: ");
-            double amount = double.Parse(Console.ReadLine());
-            bank.TransferMoney(fromAccountNumber, toAccountNumber, amount);
-            break;
-        case "5":
-            Console.Write("Enter account number: ");
-            int accountNumber = int.Parse(Console.ReadLine());
-            Account account = bank.FindAccount(accountNumber);
-            if (account != null)
+        while (!exit)
+        {
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. Deposit Money");
+            Console.WriteLine("3. Withdraw Money");
+            Console.WriteLine("4. Transfer Money");
+            Console.WriteLine("5. View Account Details");
+            Console.WriteLine("6. Exit");
+            Console.Write("Choose an option: ");
+
+            switch (Console.ReadLine())
             {
-                account.DisplayAccountDetails();
+                case "1":
+                    CreateAccount(bank);
+                    break;
+                case "2":
+                    DepositMoney(bank);
+                    break;
+                case "3":
+                    WithdrawMoney(bank);
+                    break;
+                case "4":
+                    TransferMoney(bank);
+                    break;
+                case "5":
+                    ViewAccountDetails(bank);
+                    break;
+                case "6":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Try again.");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Account not found.");
-            }
-            break;
-        case "6":
-            exit = true;
-            break;
-        default:
-            Console.WriteLine("Invalid option. Try again");
-            break;
+        }
+    }
+
+    static void CreateAccount(Bank bank)
+    {
+        Console.Write("Enter account holder name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter initial deposit: ");
+        double initialDeposit = double.Parse(Console.ReadLine());
+        bank.CreateAccount(name, initialDeposit);
+    }
+
+    static void DepositMoney(Bank bank)
+    {
+        Console.Write("Enter account number: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter amount to deposit: ");
+        double amount = double.Parse(Console.ReadLine());
+        Account account = bank.FindAccount(accountNumber);
+        if (account != null)
+        {
+            account.Deposit(amount);
+        }
+        else
+        {
+            Console.WriteLine("Account not found.");
+        }
+    }
+
+    static void WithdrawMoney(Bank bank)
+    {
+        Console.Write("Enter account number: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter amount to withdraw: ");
+        double amount = double.Parse(Console.ReadLine());
+        Account account = bank.FindAccount(accountNumber);
+        if (account != null)
+        {
+            account.Withdraw(amount);
+        }
+        else
+        {
+            Console.WriteLine("Account not found.");
+        }
+    }
+
+    static void TransferMoney(Bank bank)
+    {
+        Console.Write("Enter the source account number: ");
+        int fromAccountNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter the destination account number: ");
+        int toAccountNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter amount to transfer: ");
+        double amount = double.Parse(Console.ReadLine());
+        bank.TransferMoney(fromAccountNumber, toAccountNumber, amount);
+    }
+
+    static void ViewAccountDetails(Bank bank)
+    {
+        Console.Write("Enter account number: ");
+        int accountNumber = int.Parse(Console.ReadLine());
+        Account account = bank.FindAccount(accountNumber);
+        if (account != null)
+        {
+            account.DisplayAccountDetails();
+        }
+        else
+        {
+            Console.WriteLine("Account not found.");
+        }
     }
 }
